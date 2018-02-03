a94cf4775faf9fe480c79e81c75538c692ef0afc
Kim Bratzel <kim.bratzel@gmail.com>
2018-02-04 10:30:12 +1100

    Add Children Game implementation.

--- src/main/java/com/afl/exercises/children/ChildrenGameImpl.java
+++ src/main/java/com/afl/exercises/children/ChildrenGameImpl.java
@@ -1,13 +1,32 @@
 package com.afl.exercises.children;
 
+import static java.util.stream.Collectors.toList;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class ChildrenGameImpl implements ChildrenGame {
 
     public List<Integer> play(Integer n, Integer k) {
 
+        if (n < 2 || k < 1) {
+            throw new IllegalArgumentException("n must be at least 2. k must be at least 1.");
+        }
+
+        // List of children ids
+        List<Integer> children = IntStream.range(0, n).boxed().collect(toList());
         List<Integer> result = new ArrayList<>();
+        int removedId = 0; // Start from the 1st child
+
+        // Keep removing children until there is only 1 left
+        while (children.size() > 1) {
+            removedId = (removedId + k - 1) % children.size();
+            result.add(children.remove(removedId));
+        }
+
+        // Add the last child (the winner)
+        result.add(children.get(0));
 
         return result;
     }
